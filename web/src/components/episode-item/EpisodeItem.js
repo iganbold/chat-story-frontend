6529cbf9145f8e6df578ae77f13650f7ce1d1b57
iganbold <itgel.ganbold1@gmail.com>
2019-04-26 15:14:33 -0400

    Implement EpisodeItem presentational component

--- web/src/components/episode-item/EpisodeItem.js
+++ web/src/components/episode-item/EpisodeItem.js
@@ -1,12 +1,50 @@
 import React from "react";
 import styled from "styled-components";
+import TextDialog from "../text-dialog/";
+import ActorAvator from "../actor-avatar";
 
 const Item = styled.div`
-  background: black;
+  display: flex;
+  flex-direction: ${props =>
+    props.dialogDirection === "incoming" ? "row" : "row-reverse"};
+  align-items: flex-end;
+  margin: 10px 0px 0px;
 `;
 
-const EpisodeItem = () => {
-  return <Item>Avatar, Actor Name, Dialog goes here</Item>;
+const EpisodeItem = props => {
+  const { dialog, style } = props;
+
+  const renderDialog = (dialog, style) => {
+    switch (dialog.type) {
+      case "TEXT_DIALOG":
+        return (
+          <TextDialog
+            value={dialog.payload.value}
+            themeDialogDirection={style.dialogDirection}
+            themeTextColor={style.dialogColor}
+            themeBackgroundColor={style.dialogBackgroundColor}
+          />
+        );
+      default:
+        // TODO: need to return default dialog
+        return <TextDialog value=" " />;
+    }
+  };
+
+  const renderAvatar = style => (
+    <ActorAvator
+      themeBackgroundColor={style.avatarBackgroundColor}
+      themeDialogDirection={style.dialogDirection}
+      value={style.actorInitial}
+    />
+  );
+
+  return (
+    <Item dialogDirection={style.dialogDirection}>
+      {renderAvatar(style)}
+      {renderDialog(dialog, style)}
+    </Item>
+  );
 };
 
 export default EpisodeItem;
