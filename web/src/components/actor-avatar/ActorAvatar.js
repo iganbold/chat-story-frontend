f54b264a6f59a89bb6588f340ce486d2687a4de6
iganbold <itgel.ganbold1@gmail.com>
2019-04-26 10:34:22 -0400

    Create ActorAvatar

---
+++ web/src/components/actor-avatar/ActorAvatar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Avatar = styled.div`
+  color: green;
+`;
+
+const ActorAvatar = props => {
+  return <Avatar>J</Avatar>;
+};
+
+export default ActorAvatar;
