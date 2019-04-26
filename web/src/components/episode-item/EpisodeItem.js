a4f98a6c1dad6845642f8363679e9a55dc4e3267
iganbold <itgel.ganbold1@gmail.com>
2019-04-26 11:40:57 -0400

    Create EpisodeItem component

---
+++ web/src/components/episode-item/EpisodeItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Item = styled.div`
+  background: black;
+`;
+
+const EpisodeItem = () => {
+  return <Item>Avatar, Actor Name, Dialog goes here</Item>;
+};
+
+export default EpisodeItem;
