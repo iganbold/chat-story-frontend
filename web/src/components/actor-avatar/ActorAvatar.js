428f8be76a6e2df0a73a9950bfd39e0bea78b139
iganbold <itgel.ganbold1@gmail.com>
2019-04-26 11:12:15 -0400

    Implement ActorAvatar presentational component

--- web/src/components/actor-avatar/ActorAvatar.js
+++ web/src/components/actor-avatar/ActorAvatar.js
@@ -2,11 +2,32 @@ import React from "react";
 import styled from "styled-components";
 
 const Avatar = styled.div`
-  color: green;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  background: ${props => props.themeBackgroundColor};
+  color: #585858;
+  font-size: 13px;
+  font-weight: bold;
+  width: 25px;
+  min-width: 25px;
+  height: 25px;
+  margin-left: ${props =>
+    props.themeDialogDirection === "incoming" ? "5px" : "2.5px"};
+  margin-right: ${props =>
+    props.themeDialogDirection !== "incoming" ? "5px" : "2.5px"};
 `;
 
 const ActorAvatar = props => {
-  return <Avatar>J</Avatar>;
+  return (
+    <Avatar
+      themeBackgroundColor={props.themeBackgroundColor}
+      themeDialogDirection={props.themeDialogDirection}
+    >
+      {props.value}
+    </Avatar>
+  );
 };
 
 export default ActorAvatar;
