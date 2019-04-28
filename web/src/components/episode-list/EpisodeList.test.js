10f92c4e67842ef220c9c35654c27d7aa3b0e52d
iganbold <itgel.ganbold1@gmail.com>
2019-04-28 19:21:09 -0400

    Add scrollIntoView dummy function to html element prototype for testing

--- web/src/components/episode-list/EpisodeList.test.js
+++ web/src/components/episode-list/EpisodeList.test.js
@@ -5,6 +5,7 @@ import { episode } from "../../../tools/mockData";
 
 it("EpisodeList renders without crashing", () => {
   const div = document.createElement("div");
+  window.HTMLElement.prototype.scrollIntoView = jest.fn();
   ReactDOM.render(
     <EpisodeList
       dialogs={episode.dialogs}
