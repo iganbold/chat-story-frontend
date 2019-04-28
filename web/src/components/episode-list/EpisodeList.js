fbbe7c9d772640c4ae2b7d48bfc00a3ef9fa831a
iganbold <itgel.ganbold1@gmail.com>
2019-04-28 18:57:21 -0400

    Convert functional episode list component to class component in order to implement auto scroll to bottom

--- web/src/components/episode-list/EpisodeList.js
+++ web/src/components/episode-list/EpisodeList.js
@@ -9,28 +9,35 @@ const Space = styled.div`
   background: blue;
 `;
 
-const EpisodeList = ({ dialogs, style, onNextDialog }) => {
-  return (
-    <Box
-      flex
-      direction="column"
-      overflow="auto"
-      onClick={onNextDialog}
-      background="white"
-      pad={{
-        top: "10px"
-      }}
-    >
-      {dialogs.map((dialog, index) => (
-        <EpisodeItem
-          key={index}
-          dialog={dialog}
-          style={style[dialog.actorID]}
-        />
-      ))}
-      <Space />
-    </Box>
-  );
-};
+class EpisodeList extends React.Component {
+  handleOnClick = scrollTo => {
+    this.props.onNextDialog();
+  };
+
+  render() {
+    const { dialogs, style } = this.props;
+    return (
+      <Box
+        flex
+        direction="column"
+        overflow="auto"
+        onClick={this.handleOnClick}
+        background="white"
+        pad={{
+          top: "10px"
+        }}
+      >
+        {dialogs.map((dialog, index) => (
+          <EpisodeItem
+            key={index}
+            dialog={dialog}
+            style={style[dialog.actorID]}
+          />
+        ))}
+        <Space />
+      </Box>
+    );
+  }
+}
 
 export default EpisodeList;
