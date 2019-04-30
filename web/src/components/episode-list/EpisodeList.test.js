6f9b5521bdd02d5b3196ee1f9af087d3dd94e913
iganbold <itgel.ganbold1@gmail.com>
2019-04-30 17:11:06 -0400

    Update EpisodeItem and EpissodeList tests for ActorName componenet

--- web/src/components/episode-list/EpisodeList.test.js
+++ web/src/components/episode-list/EpisodeList.test.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import EpisodeList from "./EpisodeList";
-import { episode } from "../../../tools/mockData";
+import { actors, episode } from "../../../tools/mockData";
 
 it("EpisodeList renders without crashing", () => {
   const div = document.createElement("div");
   window.HTMLElement.prototype.scrollIntoView = jest.fn();
   ReactDOM.render(
     <EpisodeList
+      actors={actors}
       dialogs={episode.dialogs}
       style={episode.style}
       onNextDialog={jest.fn()}
