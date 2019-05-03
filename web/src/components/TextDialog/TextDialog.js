7725810a45717fd55f2b264d3263971f387b8efd
iganbold <itgel.ganbold1@gmail.com>
2019-05-03 19:42:06 -0400

    Use text Bubble component for TextDialog

--- web/src/components/TextDialog/TextDialog.js
+++ web/src/components/TextDialog/TextDialog.js
@@ -1,22 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-import AnimationDialog from "../AnimationDialog";
-
-const Bubble = styled(AnimationDialog)`
-  display: inline-block;
-  position: relative;
-  background: ${props => props.customTheme.background};
-  border-radius: ${props =>
-    props.customTheme.direction === "incoming"
-      ? "20px 20px 20px 5px"
-      : "20px 20px 5px 20px"};
-  color: ${props => props.customTheme.color};
-  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.1);
-  padding: 10px 15px;
-  max-width: 60%;
-  height: auto;
-  margin: 0px 2.5px;
-`;
+import Bubble from "../Bubble";
 
 const TextDialog = props => <Bubble {...props}>{props.label}</Bubble>;
 
