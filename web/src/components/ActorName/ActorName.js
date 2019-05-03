8a70141e078834869d4d0d6605a2d2b9d7dcf67a
iganbold <itgel.ganbold1@gmail.com>
2019-05-03 19:40:24 -0400

    Use custom theme for ActorName

--- web/src/components/ActorName/ActorName.js
+++ web/src/components/ActorName/ActorName.js
@@ -2,21 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const Name = styled.div`
-  display: ${props => (props.hide === true ? "none" : "block")};
+  display: ${props => props.customTheme.display};
   font-size: 12px;
   color: #585858;
-  margin-left: ${props =>
-    props.themeDialogDirection === "incoming" ? "45px" : "0px"};
-  margin-right: ${props =>
-    props.themeDialogDirection !== "incoming" ? "45px" : "0px"};
+  margin-left: ${props => props.customTheme.margin.left};
+  margin-right: ${props => props.customTheme.margin.right};
 `;
 
-const ActorName = props => {
-  return (
-    <Name hide={props.hide} themeDialogDirection={props.themeDialogDirection}>
-      {props.label}
-    </Name>
-  );
-};
+const ActorName = props => <Name {...props}>{props.label}</Name>;
 
 export default ActorName;
