fc60ee83190cde062fcdc9808fda339e78076519
iganbold <itgel.ganbold1@gmail.com>
2019-05-03 19:39:59 -0400

    Use custom theme fro ActorAvatar

--- web/src/components/ActorAvatar/ActorAvatar.js
+++ web/src/components/ActorAvatar/ActorAvatar.js
@@ -4,7 +4,7 @@ import BouncyKeyFrames from "../keyframes/bouncy";
 
 const Avatar = styled.div`
   display: flex;
-  visibility: ${props => (props.hide === true ? "hidden" : "visible")};
+  visibility: ${props => props.customTheme.visibility};
   justify-content: center;
   align-items: center;
   border-radius: 50%;
@@ -15,11 +15,8 @@ const Avatar = styled.div`
   width: 25px;
   min-width: 25px;
   height: 25px;
-  margin-left: ${props =>
-    props.customTheme.direction === "incoming" ? "5px" : "2.5px"};
-  margin-right: ${props =>
-    props.customTheme.direction !== "incoming" ? "5px" : "2.5px"};
-
+  margin-left: ${props => props.customTheme.margin.left};
+  margin-right: ${props => props.customTheme.margin.right};
   transform-origin: 50% 100%;
   animation: 0.3s ${BouncyKeyFrames} forwards ease;
 `;
