33aa1abca1fd4f3b0b90b5c527d621d3792f84ba
iganbold <itgel.ganbold1@gmail.com>
2019-05-03 19:40:47 -0400

    Use cutome theme from AnimationDialog

--- web/src/components/AnimationDialog/AnimationDialog.js
+++ web/src/components/AnimationDialog/AnimationDialog.js
@@ -2,9 +2,7 @@ import styled from "styled-components";
 import ExpandKeyFrames from "../keyframes/expand";
 
 const AnimationDialog = styled.div`
-  transform-origin: ${props =>
-      props.themeDialogDirection === "incoming" ? "0%" : "100%"}
-    100%;
+  transform-origin: ${props => props.customTheme.transformX} 100%;
   animation: 0.3s ${ExpandKeyFrames} forwards ease;
 `;
 
